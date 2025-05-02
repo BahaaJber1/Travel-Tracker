d7770b3066c28c5525bbbf41ad03d5542cc192a8
BahaaJber1 <bahaajber23@gmail.com>
2025-05-02 23:20:23 +0300

    fixing bug in db connection

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
-import pg from "pg";
+import { Pool } from "pg";
 dotenv.config();
 
-const db = new pg.Client({
+const db = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
@@ -12,21 +12,9 @@ const db = new pg.Client({
   port: process.env.DB_PORT,
 });
 
-db.connect();
+
 let countries = [];
 let total = 0;
-
-const result =  db.query("SELECT * FROM visited_countries", (err, res) => {
-  if (err) {
-    console.error("Error executing query", err.stack);
-  } else {
-    countries = res.rows.map((row) => row.country_code);
-    console.log("Countries fetched from database:", countries);
-    total = countries.length;
-    console.log("Total countries:", total);
-  }
-});
-db.end();
 
 const app = express();
 const port = 3000;
@@ -35,7 +23,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
-  res.render("index.ejs", { countries, total });
+  try { 
+    const result = await db.query("SELECT * FROM visited_countries");
+    countries = result.rows.map((row) => row.country_code);
+    console.log("Countries fetched from database:", countries);
+    total = countries.length;
+    console.log("Total countries:", total);
+    res.render("index.ejs", { countries, total });
+  } catch (error) {
+    console.error("Error fetching countries:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
